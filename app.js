6a3282cd00bc1f99e470a3578e239d6d99a3ce65
LaupWing <loc_strict@hotmail.com>
2020-08-23 21:39:19 +0200

    CONNECTION WORKS

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 const faker = require('faker');
+const mysql = require('mysql');
 require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD
+});
+
+console.log({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE
+})
+
+connection.query('SELECT 1 + 1 AS solution', function(err, result){
+    if(err) throw err;
+    console.log(result);
+});
+
+// connection.end()
